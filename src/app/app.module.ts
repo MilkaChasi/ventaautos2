f74311b39cae544da4a3c0870fe13dfc1f9f952b
Milka Chasi <mchasijimenez@gmail.com>
2024-03-26 21:21:47 -0500

    Proyecto Angular: Interceptores y Paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaModule } from './paginas/PaginaModule';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { UserInterceptorService } from './interceptores/UserInterceptor.service';
+import { ClientesModule } from './utilitarios/pipes/Clientes.module';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +15,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //PaginaModule,
+    //ClientesModule,
+    HttpClientModule,
+
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS, useClass: UserInterceptorService, multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
